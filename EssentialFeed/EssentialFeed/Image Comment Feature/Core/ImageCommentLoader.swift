f3aceb59dd776eb3a0d1122f593acbb19c55fbe8
Thiago Penna <thiago.penna@icloud.com>
2021-03-29 16:41:22 -0300

    Create ImageCommentLoader protocol boundary

---
+++ EssentialFeed/EssentialFeed/Image Comment Feature/Core/ImageCommentLoader.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentLoader.swift
+//  EssentialFeed
+//
+//  Created by Thiago Penna on 29/03/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public protocol ImageCommentLoader {
+	typealias Result = Swift.Result<[ImageComment], Error>
+	
+	func load(withImageId id: UUID, completion: @escaping (Result) -> Void)
+}
