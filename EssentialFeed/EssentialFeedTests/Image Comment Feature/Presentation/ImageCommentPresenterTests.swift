758788e89ab9587951570af33de5cf6efcf320af
Thiago Penna <thiago.penna@icloud.com>
2021-04-12 17:01:00 -0300

    ImageCommentPresenter displays no error on `didStartLoadingComments`

--- EssentialFeed/EssentialFeedTests/Image Comment Feature/Presentation/ImageCommentPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comment Feature/Presentation/ImageCommentPresenterTests.swift
@@ -1,6 +1,6 @@
 //
 //  ImageCommentPresenterTests.swift
-//  EssentialFeedTests
+//  EssentialImageCommentTests
 //
 //  Created by Thiago Penna on 12/04/21.
 //  Copyright © 2021 Essential Developer. All rights reserved.
@@ -8,9 +8,28 @@
 
 import XCTest
 
+public protocol ImageCommentErrorView {
+	func display(_ viewModel: ImageCommentErrorViewModel)
+}
+
+public struct ImageCommentErrorViewModel {
+	public let message: String?
+	
+	static var noError: ImageCommentErrorViewModel {
+		return ImageCommentErrorViewModel(message: nil)
+	}
+}
+
+
 public final class ImageCommentPresenter {
-	init(view: Any) {
-		
+	private let errorView: ImageCommentErrorView
+	
+	init(errorView: ImageCommentErrorView) {
+		self.errorView = errorView
+	}
+	
+	public func didStartLoadingComments() {
+		errorView.display(.noError)
 	}
 }
 	
@@ -18,12 +37,31 @@ class ImageCommentPresenterTests: XCTestCase {
 	func test_init_doesNotSendMessagesToView() {
 		let view = ViewSpy()
 		
-		_ = ImageCommentPresenter(view: view)
+		_ = ImageCommentPresenter(errorView: view)
 		
 		XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
 	}
 	
-	private class ViewSpy {
-		private(set) var messages = [Any]()
+	func test_didStartLoadingComments_displaysNoErrorMessage() {
+		let view = ViewSpy()
+		let sut = ImageCommentPresenter(errorView: view)
+		
+		sut.didStartLoadingComments()
+		
+		XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+	}
+	
+	// MARK: - Helpers
+	
+	private class ViewSpy: ImageCommentErrorView {
+		enum Message: Hashable {
+			case display(errorMessage: String?)
+		}
+		
+		private(set) var messages = [Message]()
+		
+		func display(_ viewModel: ImageCommentErrorViewModel) {
+			messages.append(.display(errorMessage: viewModel.message))
+		}
 	}
 }
